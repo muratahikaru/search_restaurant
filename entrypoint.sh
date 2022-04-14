b06fe60e71adfe2d22da4d2e0c3ce8714681e635
村田光 <your.muhi_1122@icloud.com>
2022-04-14 17:43:01 +0900

    add: 環境構築

---
+++ entrypoint.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+set -e
+
+# Remove a potentially pre-existing server.pid for Rails.
+rm -f /search_restaurant/tmp/pids/server.pid
+
+if [ "${RAILS_ENV}" = "production" ]
+then
+  bundle exec rails assets:precompile
+fi
+
+# Then exec the container's main process (what's set as CMD in the Dockerfile).
+exec "$@"
