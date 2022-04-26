0f78c0fe41b6bd2548aced76c408e1d4521d99b8
村田光 <your.muhi_1122@icloud.com>
2022-04-26 17:03:08 +0900

    add: レストラン詳細画面実装

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -10,7 +10,7 @@ class RestaurantsController < ApplicationController
     latitude = params[:latitude]
     longitude = params[:longitude]
 
-    params = URI.encode_www_form({ key: key, lat: latitude, lng: longitude, range: range,})
+    params = URI.encode_www_form({ key: key, lat: latitude, lng: longitude, range: range})
 
     uri = URI.parse("http://webservice.recruit.co.jp/hotpepper/gourmet/v1/?#{params}")
     response = Net::HTTP.start(uri.host, uri.port) do |http|
@@ -24,10 +24,27 @@ class RestaurantsController < ApplicationController
       hash = Hash.from_xml(xml.elements['results'].to_s)
 
       @restaurants = hash["results"]["shop"]
+    end
 
-      # hash["results"]["shop"].each do |shop|
-      #   logger.debug(shop["id"])
-      # end
+  end
+
+  def show
+    id = params[:id]
+    key = "3012a463ad450b03"
+
+    params = URI.encode_www_form({ key: key, id: id})
+    uri = URI.parse("http://webservice.recruit.co.jp/hotpepper/gourmet/v1/?#{params}")
+    response = Net::HTTP.start(uri.host, uri.port) do |http|
+      http.get(uri.request_uri)
+    end
+
+    case response
+    when Net::HTTPSuccess
+      xml = REXML::Document.new(response.body)
+
+      hash = Hash.from_xml(xml.elements['results'].to_s)
+
+      @restaurant = hash["results"]["shop"]
     end
 
   end
