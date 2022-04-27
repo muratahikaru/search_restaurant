ad618353bf8e17a8a21076a0229d9bbc3d406b2a
村田光 <your.muhi_1122@icloud.com>
2022-04-28 06:16:08 +0900

    update:　変数の初期化を条件分岐の中に入れる

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -10,17 +10,17 @@ class RestaurantsController < ApplicationController
   end
 
   def index
-    key = "3012a463ad450b03"
-    range = params[:range]
-    latitude = params[:latitude]
-    longitude = params[:longitude]
-    number_of_output = 100
     current_page = params[:page] || 1
-    params = URI.encode_www_form({ key: key, lat: latitude, lng: longitude, range: range, count: number_of_output})
 
     if Rails.cache.exist?('restaurants')
       @restaurants = Kaminari.paginate_array(Rails.cache.read('restaurants')).page(current_page).per(10)
     else
+      key = "3012a463ad450b03"
+      range = params[:range]
+      latitude = params[:latitude]
+      longitude = params[:longitude]
+      number_of_output = 100
+      params = URI.encode_www_form({ key: key, lat: latitude, lng: longitude, range: range, count: number_of_output})
       uri = URI.parse("http://webservice.recruit.co.jp/hotpepper/gourmet/v1/?#{params}")
       response = Net::HTTP.start(uri.host, uri.port) do |http|
         http.get(uri.request_uri)
